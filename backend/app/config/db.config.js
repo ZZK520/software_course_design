1438bce572a39274c45e352cc35ee35380432fd5
朱志康 <752330461@qq.com>
2021-03-08 22:55:56 +0800

    本地可以访问服务器

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  url: "mongodb://localhost:27071/software_cours_design"
-};
+const DB_NAME='softwareCD'
+const DB_BASEURL_LOCAL='localhost' 
+const DB_BASEURL_ONLINE='106.13.182.155' 
+
+const DB_PORT_ONLINE='27071';//远程连接
+const DB_PORT_LOCAL='27027';//本地连接
+
+const USERNAME='softwareCDAdmin';
+const PASSWORD='softwareCDPWD';
+const ONLINE_CONFI=`mongodb://${USERNAME}:${PASSWORD}@${DB_BASEURL_ONLINE}:${DB_PORT_ONLINE}/${DB_NAME}`//生产环境，线上
+const LOCAL_CONFI=`mongodb://${DB_BASEURL_LOCAL}:${DB_PORT_LOCAL}/${DB_NAME}`//开发环境,本地，无密码可登录
+
+exports.ONLINE_CONFI=ONLINE_CONFI;
+exports.LOCAL_CONFI=LOCAL_CONFI;
+
